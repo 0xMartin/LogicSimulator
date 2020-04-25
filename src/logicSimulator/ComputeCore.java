3b0fef60420c34bb38d9b07632e28e055cacda5f
0xMartin <0xMartin@users.noreply.github.com>
2020-04-25 23:11:33 +0200

    Documentation editor, New components, Hex editor improved (2) fix
    New layout manager for toolbars, New icons, changed design of propertie editor, variable count of inputs for (AND, OR, XOR, NAND, NOR, NXOR),
    
    New components:
    MUX, DMUX,  Keyboard, 7 SEG, Text screen, Magnituge comparator, Binary Add, Binary Sub, Binary Mul, Binary Div, Binary constant, RWM SAM, RWM RAM, RS flip flop, JK flip flop, D flip flop, T flip flop, Register, LIFO stack, FIFO stack
    
    Hex editor :
    New features - convert dec number to hex, upload code to memory, memory chooser, Program offset, Clear option, Debug option, (highlight current command), comment, decomment, translation  configration
    
    Documentation editor:
    Features - Font chooser, size of font, foreground color, background color, bold, italic, underline, left align, center align, right align, justify align, table (editing not work),  horisontal separator, list dot, sub script, super script, strike throught

--- src/logicSimulator/ComputeCore.java
+++ src/logicSimulator/ComputeCore.java
@@ -1,6 +1,18 @@
-/*
- * Logic simlator
- * Author: Martin Krcma
+/* 
+ * Copyright (C) 2020 Martin Krcma
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package logicSimulator;
 
@@ -10,7 +22,6 @@ import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.List;
-import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import logicSimulator.objects.IOPin;
